b7b3dc48d94561302e3355a6aa8643a3979c8f2f
Ryan <ryankanno@localkinegrinds.com>
2011-05-13 11:15:34 -1000

    Added rss_cron.js Crawls rss feed every 30 seconds

--- rss_cron_to_redis/rss_cron.js
+++ rss_cron_to_redis/rss_cron.js
@@ -1,7 +1,34 @@
 var express = require('express'),
 	  OAuth = require('oauth').OAuth,
 	     qs = require('querystring'),
+       cron = require('cron'),
+    request = require('request'),
+ htmlparser = require('htmlparser'),
+        sys = require('sys'),
        port = 8000;
+
+var RSS_FEED = process.ARGV[2];
+
+if (!RSS_FEED)
+	return sys.puts("Usage: node rss_cron.js <rss_feed>");
+
+var rss_handler = new htmlparser.RssHandler(function (error, dom) {
+    console.log(dom.items);
+});
+
+var parser = new htmlparser.Parser(rss_handler);
+
+new cron.CronJob('*/30 * * * * *', function() {
+    get_feed(RSS_FEED);
+});
+
+function get_feed(rss_feed) {
+    request({uri:rss_feed}, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            parser.parseComplete(body);
+        }
+    })
+};
 
 var app = express.createServer();
 
