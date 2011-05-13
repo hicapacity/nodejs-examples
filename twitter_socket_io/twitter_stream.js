bb716530916349549d7d155958f3201a8212501c
Ryan Kanno <ryankanno@localkinegrinds.com>
2011-05-13 08:20:14 -1000

    Updated twitter_socket_io changes

--- twitter_socket_io/twitter_stream.js
+++ twitter_socket_io/twitter_stream.js
@@ -1,20 +1,16 @@
 var sys = require("sys"),
    http = require("http"),
     url = require("url"), 
-   path = require("path"), 
-     fs = require("fs"), 
- events = require("events"), 
  static = require("node-static"),
      io = require("socket.io"),
 Twitter = require("twitter-node").TwitterNode,
-   port = 8000,
-  delay = 5000;
+   port = 8000;
 
 var USERNAME = process.ARGV[2];
 var PASSWORD = process.ARGV[3];
 
 if (!USERNAME || !PASSWORD)
-	return sys.puts("Usage: node server.js <twitter_username> <twitter_password>");
+	return sys.puts("Usage: node twitter_stream.js <twitter_username> <twitter_password>");
 
 var twit = new Twitter({
 	user: USERNAME, 
@@ -23,7 +19,7 @@ var twit = new Twitter({
 	locations: [-157.86, 21.31, -156.86, 22.31] // Track Hawaii
 });
 
-var file = new (static.Server)('./public');
+var file = new static.Server('./public');
 
 var server = http.createServer(function (request, response) {
 	var uri = url.parse(request.url).pathname;
