625c566a3dbb207015e142d69d693371767be03e
James Wang <jianshi.wang@gmail.com>
2011-06-27 19:26:38 -1000

    Specified versions in install.sh. Added error checking and updated twitter_socket_io to use emit instead of broadcast to conform to the twitter-node api change.

--- install.sh
+++ install.sh
@@ -4,15 +4,15 @@
 type -P npm &>/dev/null || { echo ">>> NPM not installed. Please install using: curl http://npmjs.org/install.sh | sh" >&2; exit 1; }
 
 # Install dependencies (too lazy to figure out exact versions)
-npm install express
-npm install request
-npm install redis
-npm install socket.io
-npm install node-static
-npm install oauth
-npm install cron
-npm install htmlparser
-npm install twitter-node
+npm install express@2.3.12
+npm install request@1.9.8
+npm install redis@0.6.0
+npm install socket.io@0.7.2
+npm install node-static@0.5.6
+npm install oauth@0.9.1
+npm install cron@0.1.2
+npm install htmlparser@1.7.3
+npm install twitter-node@0.0.2
 
 # Create symlink in all directories
 DIRS=$(find `pwd` -d 1 -type d \( -not -iname ".*" -and -not -iname "node_modules" \))
